1f9f18a665b3052e54b0ec7d4e6f46a69f82e471
cao <cao_cgq@home.com>
2021-04-15 22:16:04 +0800

    modify

--- cmake-build-debug/CMakeFiles/C_s.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/C_s.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "D:/WorkProject/C/C_Basic/main.c" "D:/WorkProject/C/C_Basic/cmake-build-debug/CMakeFiles/C_s.dir/main.c.obj"
+  "D:/workproject/CLionProjects/C_Basic/main.c" "D:/workproject/CLionProjects/C_Basic/cmake-build-debug/CMakeFiles/C_s.dir/main.c.obj"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "MSVC")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
--- cmake-build-debug/CMakeFiles/C_s.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/C_s.dir/cmake_clean.cmake
@@ -2,8 +2,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/C_s.dir/main.c.obj"
   "C_s.exe"
   "C_s.exe.manifest"
+  "C_s.lib"
   "C_s.pdb"
-  "libC_s.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
